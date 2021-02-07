da738233de23cea257b129faf3d2a4bdaea31ad9
EvilMadSquirrel <minichev.s.l@gmaul.com>
2021-02-07 16:47:51 +0300

    added contacts page

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my Amazing Gallery!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my Amazing Gallery!</h1>")
+	} else if r.URL.Path == "/contacts" {
+		fmt.Fprint(w, "To get in touch please send an email to: <a href=\"mailto:minichev.s.l@gmail.com\":>minichev.s.l@gmail.com</a>")
+	}
 }
 
 func main() {
